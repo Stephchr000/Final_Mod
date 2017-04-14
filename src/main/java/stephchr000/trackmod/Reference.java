928b66f6ed4d389b0f936999f0d74e9fdd24acd8
stephchr000 <stephchr000@warren.k12.in.us>
2017-04-14 11:14:17 -0400

    Initial

--- src/main/java/stephchr000/trackmod/Reference.java
+++ src/main/java/stephchr000/trackmod/Reference.java
@@ -9,7 +9,7 @@ public class Reference {
     public static final String VERSION = "1.0";
     public static final String ACCEPTED_VERSIONS = "[1.11.2]";
 
-    public static final String CLIENT_PROXY_CLASS = "stephchr000.trackmod.proxy.ClientProxy";
-    public static final String SERVER_PROXY_CLASS = "stephchr000.trackmod.proxy.ServerProxy";
+    public static final String CLIENT_PROXY_CLASS = "stephchr000.trackmod.stephchr000.trackmod.proxy.ClientProxy";
+    public static final String SERVER_PROXY_CLASS = "stephchr000.trackmod..stephchr000.trackmod.proxy.ServerProxy";
 
 }
--- src/main/java/stephchr000/trackmod/TrackMod.java
+++ src/main/java/stephchr000/trackmod/TrackMod.java
@@ -1,5 +1,7 @@
 package stephchr000.trackmod;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.Mod.Instance;
 import net.minecraftforge.fml.common.SidedProxy;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
